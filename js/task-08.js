ddbe0da5ec0def43187648179c900d79800a84f3
alinabihdan <alina.bigdan1@gmail.com>
2021-05-19 16:40:06 +0300

    изменяет 8 задачу

--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,13 @@
 // Напиши скрипт создания и очистки коллекции элементов.Пользователь вводит количество элементов в input и нажимает кнопку
 // Создать, после чего рендерится коллекция.При нажатии на кнопку Очистить, коллекция элементов очищается.
-
 // Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число.Функция создает столько div, сколько указано
 // в amount и добавляет их в div#boxes.
-
 // Каждый созданный div:
-
 // Имеет случайный rgb цвет фона
 // Размеры самого первого div - 30px на 30px
 // Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
 
-// <div id="controls">
-//   <input type="number" min="0" max="100" step="1" />
-//   <button type="button" data-action="render">Создать</button>
-//   <button type="button" data-action="destroy">Очистить</button>
-// </div>
-
-// <div id="boxes"></div>
 const refs = {
   input: document.querySelector('[type="number"]'),
   btnRender: document.querySelector('[data-action="render"]'),
@@ -42,12 +32,14 @@ function createBoxes(amount) {
     newDiv.style.backgroundColor = randomColor();
     array.push(newDiv);
   }
+
   return refs.boxesRef.append(...array);
 }
 
 function destroyBoxes() {
   refs.boxesRef.innerHTML = "";
   refs.input.value = "";
+  amount = 0;
 }
 
 const randomColor = () => {
@@ -55,5 +47,8 @@ const randomColor = () => {
 };
 
 // слушатели
-refs.btnRender.addEventListener("click", createBoxes);
+refs.btnRender.addEventListener("click", () => {
+  createBoxes(amount);
+  refs.input.value = "";
+});
 refs.btnDestroy.addEventListener("click", destroyBoxes);
